d19e51cb99e405cf93a20833061808c48b0cede4
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-01-15 19:09:14 +0300

    feature /  themeSwitcher

---
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,24 @@
+import styles from './ThemeSwitcher.module.scss';
+import {ReactComponent as IconMoon} from 'assets/icon-moon.svg'
+import {ReactComponent as IconSun} from 'assets/icon-sun.svg'
+import {useEffect, useState} from 'react';
+
+
+export const ThemeSwitcher = () => {
+    const [isDark, setIsDark] = useState(false)
+    const themeText = isDark ? 'Light' : 'Dark';
+    const ThemeIcon = isDark ? IconSun : IconMoon
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', isDark ? 'dark' : 'light')
+    }, [isDark])
+
+    return (
+        <div className={styles.switcher} onClick={() => setIsDark(!isDark)}>
+            <span>{themeText}</span>
+            <ThemeIcon className={styles.icon}/>
+        </div>
+    )
+}
+
+
