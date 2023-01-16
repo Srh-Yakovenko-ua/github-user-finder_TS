166c886c89c70fde1d7d6d48edfcda32b22857d6
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-01-16 23:41:41 +0300

    another refactor

--- src/types/users.ts
+++ src/types/users.ts
@@ -1,39 +1,36 @@
 //https://api.github.com/users/Srh-Yakovenko-ua
 
-
 export type LocalGithubUser = {
-    login : string
-    id : number
-    avatar : string
-    name : string
-    company : string
-    blog : string
-    location : string
-    bio : string
-    twitter : string
-    repos : number
-    followers : number
-    following : number
-    created_at  : string
+  login: string
+  avatar: string
+  name: string
+  company: string | null
+  blog: string
+  location: string
+  bio: string
+  twitter: string | null
+  repos: number
+  followers: number
+  following: number
+  created_at: string
 }
 
 export type GithubUser = {
-    login : string
-    id : number
-    avatar_url : string
-    name : string
-    company : string
-    blog : string
-    location : string
-    bio : string
-    twitter_username : string
-    public_repos : number
-    followers : number
-    following : number
-    created_at  : string
+  login: string
+  avatar_url: string
+  name: string
+  company: string | null
+  blog: string
+  location: string
+  bio: string
+  twitter_username: string | null
+  public_repos: number
+  followers: number
+  following: number
+  created_at: string
 }
 
 export type GithubError = {
-    message : string
-    documentation_url : string
+  message: string
+  documentation_url: string
 }
